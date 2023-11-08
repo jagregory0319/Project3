2b15937eb7aebda3ed5ba782c87f3577b010d83e
jagregory0319 <143538384+jagregory0319@users.noreply.github.com>
2023-11-08 18:55:21 +0000

    Added workflow

---
+++ documentation.py
@@ -0,0 +1,37 @@
+
+#Data Cleaning and Wrangling 
+
+
+
+
+
+
+
+ # Graph visualization of breakdown of activities and number of days for each 
+
+
+
+
+
+ # Graph visualization of frequency of activities over certain periods of time
+
+
+
+
+
+ # Graph visualization of performance improvement/regression over period of time 
+
+
+
+
+
+ #  Predictive model to estimate times for upcoming races based on previous performance
+
+
+
+
+# Graph visualization of predictive model.
+
+
+
+
