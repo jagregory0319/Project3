f74a4cb86a818bfdb4c60814deaf6eec03e16d57
Mr-Kru <120538325+Mr-Kru@users.noreply.github.com>
2023-11-15 11:51:22 -0500

    Update documentation.py

--- documentation.py
+++ documentation.py
@@ -6,17 +6,37 @@ df=pd.read_csv ('Running log.csv')
 #Parsing Date Column
 df['Date'] = pd.to_datetime(df['Date'])
 df['Date'] = df['Date'].dt.date
+
 #Filter specefied rows 
 
-
-
-
-
+#Removing last 18 rows of data
+df = df.iloc[:-18]
+#Selecting the columns
+columns_to_keep = ['Activity Type', 'Date', 'Title', 'Distance', 'Time', 'Avg HR', 'Avg Pace', 'Total Ascent']
+df_cleaned = df[columns_to_keep]
 
 
  # Graph visualization of breakdown of activities and number of days for each 
 
+activity_counts = df_cleaned['Activity Type'].value_counts()
 
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10, 8))
+
+ax = activity_counts.plot(kind='bar', color='skyblue')
+
+for p in ax.patches:
+    ax.annotate(str(p.get_height()), (p.get_x() + p.get_width() / 2., p.get_height()),
+                ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+
+plt.title('Number of Activities by Type')
+plt.xlabel('Activity Type')
+plt.ylabel('Number of Activities')
+
+plt.xticks(rotation=45, ha='right')
+
+plt.tight_layout()
+plt.show()
 
 
 
